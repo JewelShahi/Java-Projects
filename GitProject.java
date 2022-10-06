67039d63ec27284a309ca2ae5f6f8739af3e162a
JewelShahi <115151017+JewelShahi@users.noreply.github.com>
2022-10-06 20:59:07 +0300

    Update GitProject.java

--- GitProject.java
+++ GitProject.java
@@ -1,11 +1,11 @@
-package git;
+package pack;
+import java.util.*;
 
 public class GitProject {
-
 	public static void main(String[] args) {
 		String str="Hello github!";
 		for(int i=0; i<str.length(); i++)
 			System.out.print(str.charAt(i)+" ");
 	}
-
 }
+
