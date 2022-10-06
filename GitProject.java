9b6d6b4048def3b5f13f25b59c0802a7ee367119
JewelShahi <115151017+JewelShahi@users.noreply.github.com>
2022-10-06 20:57:44 +0300

    Add files via upload

---
+++ GitProject.java
@@ -0,0 +1,11 @@
+package git;
+
+public class GitProject {
+
+	public static void main(String[] args) {
+		String str="Hello github!";
+		for(int i=0; i<str.length(); i++)
+			System.out.print(str.charAt(i)+" ");
+	}
+
+}
