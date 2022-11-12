e476aeccb9b0c778deaf539098b122f4f6474acf
JoeTheBest <115151017+JewelShahi@users.noreply.github.com>
2022-11-12 22:07:34 +0200

    Update FindOutlier.java

--- FindOutlier.java
+++ FindOutlier.java
@@ -1,7 +1,9 @@
-//sum of numbers from 1 to n
-import java.util.Scanner;
+/*
+
+*/
+import java.util.*;
 public class FindOutlier{
-  static int find(int[] integers){
+  public static int findOdd(int[] integers){
     int odd=0, even=0, result = 0;
     for(int i=0; i<integers.length; i++){
       if(integers[i]%2==0) even++;
@@ -16,6 +18,23 @@ public class FindOutlier{
         if(i%2==0) result=i;
       }
     }
-    
-  return result;
-}}
+    return result;
+  }
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter how many numbers to add:");
+    int n = scan.nextInt();
+    int[] integers = new int[n];
+    for(int i=0; i<n; i++){
+      integers[i]=scan.nextInt();
+    }
+    String saver = "[";
+    for(int i=0; i<integers.length; i++){
+      if(!(i+1==integers.length-1))
+        saver+=Integer.toString(i)+", ";
+      else 
+        saver+=Integer.toString(i)+"]";
+    }
+    System.out.println("The odd number from the array "+saver+" is "+findOdd(integers));
+  }
+}
