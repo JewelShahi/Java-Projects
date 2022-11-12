7fcbac423af614706509b0c846c2754b302772af
JoeTheBest <115151017+JewelShahi@users.noreply.github.com>
2022-11-12 21:33:44 +0200

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
 class Vowel{
-  public static int getCount(String str) {
-    int count=0;
-    for(int i=0; i<str.length(); i++){
-      switch(str.charAt(i)){
-          case 'a': case 'A': case 'e': case 'E': case 'i': case 'I':  case 'o': case 'O': case 'u': case 'U':
-            count++;
-      }
-    }
-    return count;
-  }
-  public static void main(String[] args){
-	Scanner scan = new Scanner(System.in);
-	String str = scan.nextLine();
-	System.out.println("Vowels: "+getCount(str));
-	scan.close();
-  }
+	public static int getCount(String str) {
+		int count=0;
+		for(int i=0; i<str.length(); i++){
+			switch(str.charAt(i)){
+				case 'a': case 'A': case 'e': case 'E': case 'i': case 'I':  case 'o': case 'O': case 'u': case 'U':
+					count++;
+			}
+		}
+		return count;
+	}
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		String str = scan.nextLine();
+		System.out.println("Vowels: "+getCount(str));
+		scan.close();
+	}
 }
