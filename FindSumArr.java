a17649a9b351bd0a2b47a36828be43790625d51c
JoeTheBest <115151017+JewelShahi@users.noreply.github.com>
2022-11-17 23:26:38 +0200

    Create FindSumArr.java

---
+++ FindSumArr.java
@@ -0,0 +1,22 @@
+class FindSumArr{
+	public static int[] Add(int n[], int tar){
+		int saverArr[]=new int[2];// making an array to save the indexes
+
+		// finding from the vector numbers that are equal to entered number from the user
+		for(int i=0; i<n.length; i++){
+      		for(int j=0; j<n.length; j++){
+      			if(n[i]+n[j]==tar&&i!=j){// the number musn't be it self
+            			saverArr[0]=i;
+            			saverArr[1]=j;
+            			break;
+          			}
+        		}
+      	}
+		return saverArr;
+	}
+
+	public static void main(String[] args){
+    		int a[]={4, 5, 1, 8, 2, 7};
+      	System.out.println(Add(a, 5)[0]+" "+Add(a, 5)[1]);
+	}
+}
