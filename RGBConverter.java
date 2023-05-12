78b7c9b3e3fd5b7c647153a9731f3de944ed5310
JoeTheBest <115151017+JewelShahi@users.noreply.github.com>
2023-05-12 15:34:57 +0300

    Update RGBConverter.java

--- RGBConverter.java
+++ RGBConverter.java
@@ -6,27 +6,27 @@ class RGBConverter {
     String saver="";
 
     // if the values are negative
-    r=r<0?0:r;
-    g=g<0?0:g;
-    b=b<0?0:b;
+    r = (r<0) ? 0:r;
+    g = (g<0) ? 0:g;
+    b = (b<0) ? 0:b;
 
     // if the values are bigger than 255
-    r=r<=255?r:255;
-    g=g<=255?g:255;
-    b=b<=255?b:255;
+    r = (r<=255) ? r:255;
+    g = (g<=255) ? g:255;
+    b= (b<=255) ? b:255;
 
     // concating the values
-    saver+=Hexadecimal(r);
-    saver+=Hexadecimal(g);
-    saver+=Hexadecimal(b);
+    saver += Hexadecimal(r);
+    saver += Hexadecimal(g);
+    saver += Hexadecimal(b);
 
     // returning the exct for of hex value in programming language
-    return "0x"+saver;
+    return "0x" + saver;
   }
 
   // remainder converter
   public static String toHexVal(int r) {
-    switch(r){
+    switch (r){
       case 10:
         return "A";
       case 11:
@@ -45,8 +45,8 @@ class RGBConverter {
 
   // reversing the string
   public static String backWords(String s) {
-    String saver="";
-    for (int i=s.length()-1; i>=0; i--) {
+    String saver = "";
+    for (int i = s.length()-1; i >= 0; i--) {
       saver+=s.charAt(i);
     }
     return saver;
@@ -55,26 +55,26 @@ class RGBConverter {
   // hex converter
   public static String Hexadecimal(int n) {
     String str="";
-    int d=0, r=0;
+    int d = 0, r = 0;
     do{
-      if (n>=16) {
-        d=n/16;
-        r=n-d*16;
-        n=d;
-        str+=toHexVal(r);
+      if (n >= 16) {
+        d = n/16;
+        r = n-d*16;
+        n = d;
+        str += toHexVal(r);
       }else if(n<16){
-        if(n!=0){
-          r=n;
-          n=0;
-          str+=toHexVal(r);
+        if(n != 0){
+          r = n;
+          n = 0;
+          str += toHexVal(r);
         }else{
-          str+="0";
+          str += "0";
         }
       }
     }while(n!=0);
-    str=backWords(str);
+    str = backWords(str);
     if(str.length()<2){
-      str="0"+str;
+      str = "0" + str;
     }
     return str;
   }
